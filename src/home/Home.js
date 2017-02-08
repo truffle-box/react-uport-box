2923f95548a21b4446730f4c961b17c0cff8272b
Josh Quintal <josh@zoyescreative.com>
2017-02-08 16:58:44 -0500

    Added react-router, redux, UPort integration.

---
+++ src/home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return(
+      <main className="container">
+        <div className="pure-g">
+          <div className="pure-u-1-1">
+            <h1>Good to Go!</h1>
+            <p>Your Truffle Box is installed and ready.</p>
+            <h2>UPort Authentication</h2>
+            <p>Some text about the UPort integration.</p>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default Home
---
+++ src/util/wrappers.js
@@ -0,0 +1,17 @@
+import { UserAuthWrapper } from 'redux-auth-wrapper'
+import { routerActions } from 'react-router-redux'
+
+export const UserIsAuthenticated = UserAuthWrapper({
+  authSelector: state => state.user.data,
+  redirectAction: routerActions.replace,
+  wrapperDisplayName: 'UserIsAuthenticated'
+})
+
+export const UserIsNotAuthenticated = UserAuthWrapper({
+  authSelector: state => state.user,
+  redirectAction: routerActions.replace,
+  failureRedirectPath: (state, ownProps) => ownProps.location.query.redirect || '/dashboard',
+  wrapperDisplayName: 'UserIsNotAuthenticated',
+  predicate: user => user.data === null,
+  allowRedirectBack: false
+})
