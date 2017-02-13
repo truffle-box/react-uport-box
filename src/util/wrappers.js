22316c4b5ce0560cb0caba9a61dc333d448bbd6c
Josh Quintal <josh@zoyescreative.com>
2017-02-13 11:57:31 -0500

    Updated favicon and added component wrappers.

--- src/util/wrappers.js
+++ src/util/wrappers.js
@@ -1,6 +1,8 @@
 import { UserAuthWrapper } from 'redux-auth-wrapper'
 import { routerActions } from 'react-router-redux'
 
+// Layout Component Wrappers
+
 export const UserIsAuthenticated = UserAuthWrapper({
   authSelector: state => state.user.data,
   redirectAction: routerActions.replace,
@@ -15,4 +17,20 @@ export const UserIsNotAuthenticated = UserAuthWrapper({
   wrapperDisplayName: 'UserIsNotAuthenticated',
   predicate: user => user.data === null,
   allowRedirectBack: false
+})
+
+// UI Component Wrappers
+
+export const VisibleOnlyAuth = UserAuthWrapper({
+  authSelector: state => state.user,
+  wrapperDisplayName: 'VisibleOnlyAuth',
+  predicate: user => user.data,
+  FailureComponent: null
+})
+
+export const HiddenOnlyAuth = UserAuthWrapper({
+  authSelector: state => state.user,
+  wrapperDisplayName: 'HiddenOnlyAuth',
+  predicate: user => user.data === null,
+  FailureComponent: null
 })
